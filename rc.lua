ea70bccd309fb05863447b938129cb6325a3fa6c
Henning Morud <h.morud@sportradar.com>
2016-02-17 14:49:57 +0100

    Use 'x-terminal-emulator' to start terminal

--- rc.lua
+++ rc.lua
@@ -51,7 +51,7 @@ end
 beautiful.init(awful.util.getdir("config") .. "/themes/molokai/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "gnome-terminal"
+terminal = "x-terminal-emulator"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
